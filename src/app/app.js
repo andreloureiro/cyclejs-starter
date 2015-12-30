77149985f2e68dc41416be0cbfc0bfbcccfe52fd
Andre Loureiro <andreloureiroo@gmail.com>
2015-12-30 00:04:31 -0200

    Upgraded to Component concept

--- src/app/app.js
+++ src/app/app.js
@@ -1,23 +1,63 @@
-import Rx from 'rx';
+import {Observable} from 'rx';
 import Cycle from '@cycle/core';
-import {makeDOMDriver, h} from '@cycle/dom';
+import {div, input, h1, makeDOMDriver} from '@cycle/dom';
 
-const title = 'Cycle.js Starter';
 
-function main (responses) {
+// Adjective Input
+
+function AdjectiveInput(sources) {
+
+  const inputValue$ = sources.DOM
+    .select('#adjectiveInput')
+    .events('input')
+    .map(e => e.target.value)
+    .startWith('');
+
+  const vtree$ = inputValue$
+    .map(value =>
+      div([
+        h1(`Cycle is ${value}`),
+        input('#adjectiveInput', {
+          type: 'text'
+        })
+      ])
+    );
+
+  const sinks = {
+    DOM: vtree$,
+    inputValue$
+  };
+
+  return sinks;
+}
+
+
+// Top Level
+
+function main(responses) {
+
+  const title$ = Observable.just('Cycle. js Starter');
+
+  const adjectiveInput = AdjectiveInput({DOM: responses.DOM});
+  const adjectiveInputVTree$ = adjectiveInput.DOM;
+  const adjectiveInputValue$ = adjectiveInput.inputValue$;
+
+  const vTree$ = Observable
+    .combineLatest(
+      title$,
+      adjectiveInputVTree$,
+      (title, inputVTree) =>
+        div([
+          inputVTree
+        ])
+    );
 
   return {
-    DOM: Rx.Observable
-      .just(title)
-      .map(title =>
-        h('h1', title)
-      )
-  }
+    DOM: vTree$
+  };
 }
 
-let drivers = {
+
+Cycle.run(main, {
   DOM: makeDOMDriver('#app')
-}
-
-Cycle.run(main, drivers);
-
+});
