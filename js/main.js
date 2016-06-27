790c0fca8ba835fdb3be9d3157ae33119fb5b3e1
Andre Loureiro <andreloureiroo@gmail.com>
2016-06-27 23:24:34 +0200

    Add Hello World example

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ import {makeDOMDriver} from '@cycle/dom'
 import {isolate} from '@cycle/isolate'
 import {restart, restartable} from 'cycle-restart'
 import BMI from './examples/bmi'
+import Hello from './examples/hello-world'
 
 const drivers = {
   DOM: makeDOMDriver('#root')
