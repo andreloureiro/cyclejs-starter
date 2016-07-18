5849e5a5563bb92b9c1fa66dd7fd787232b8e7df
Andre Loureiro <andreloureiroo@gmail.com>
2016-07-18 09:51:42 +0200

    Add missing semicolons

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
-import {run} from '@cycle/xstream-run'
-import {makeDOMDriver} from '@cycle/dom'
-import {isolate} from '@cycle/isolate'
-import {restart, restartable} from 'cycle-restart'
-import BMI from './examples/bmi'
-import Hello from './examples/hello-world'
+import {run} from '@cycle/xstream-run';
+import {makeDOMDriver} from '@cycle/dom';
+import BMI from './examples/bmi';
+import Hello from './examples/hello-world';
 
 const drivers = {
   DOM: makeDOMDriver('#root')
 };
 
-run(BMI, drivers)
+run(BMI, drivers);
